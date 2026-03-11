dd17d8c2fd4eb0c7db944b17fe18f2ebc869a2e3
Mihaela <mihaela.untu@gmail.com>
2026-03-11 17:40:13 +0200

    init: scaffold go2web project structure with argument parser

--- go2web.py
+++ go2web.py
@@ -8,8 +8,9 @@ def build_parser():
     parser = argparse.ArgumentParser(
         prog="go2web",
         description="A CLI tool to make raw HTTP requests and search the web.",
-        add_help=False,  # we handle -h manually for full control
+        add_help=False,
     )
+    parser.add_argument("-h", dest="show_help", action="store_true", help="Show this help message and exit")
     parser.add_argument("-u", metavar="URL", help="Make an HTTP request to URL and print the response")
     parser.add_argument("-s", metavar="SEARCH_TERM", nargs="+", help="Search for SEARCH_TERM and print top 10 results")
     return parser
@@ -32,9 +33,9 @@ Examples:
 
 def main():
     parser = build_parser()
-    args, unknown = parser.parse_known_args()
+    args = parser.parse_args()
 
-    if args.h or len(sys.argv) == 1 or "-h" in unknown:
+    if args.show_help or len(sys.argv) == 1:
         print_help()
         sys.exit(0)
 
